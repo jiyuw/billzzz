5037398a46c04c11f462ceb007ce6977e85d19f5
Proter33 <>
2026-01-16 22:29:08 +0100

    Drizzle migration fix for 0005_add_analytics_fields.sql

--- drizzle/migrations/0005_add_analytics_fields.sql
+++ drizzle/migrations/0005_add_analytics_fields.sql
@@ -1,4 +1,6 @@
 -- Add analytics fields to user_preferences table
 ALTER TABLE `user_preferences` ADD `expected_income_amount` real;
+--> statement-breakpoint
 ALTER TABLE `user_preferences` ADD `current_balance` real;
+--> statement-breakpoint
 ALTER TABLE `user_preferences` ADD `last_balance_update` integer;
